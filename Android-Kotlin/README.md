b37f9a3ed62dae0a6b4ca73d5480cd75b518b3a5
G9 <2202insane@gmail.com>
2018-09-21 18:45:11 +0700

    move from personal repo

---
+++ Android-Kotlin/README.md
@@ -0,0 +1,4 @@
+# About This Project
+
+## I Made This project for learn basic of [kotlin-android](https://github.com/topics/kotlin-android)
+## Teach Ma friends about [Laravel](https://laravel.com/)
---
+++ Android-Kotlin/app/src/main/java/G9/cooem/id/MainActivity.kt
@@ -0,0 +1,12 @@
+package g9.cooem.id
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
